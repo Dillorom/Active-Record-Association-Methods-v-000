fc1841c92428f50b2b6aba862c2583098d7528b2
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-27 20:55:40 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,12 +4,12 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    #binding.pry
     #return the genre of the artist's first saved song
     self.songs.first.genre_id
   end
 
   def song_count
+    binding.pry
     #return the number of songs associated with the artist
     self.songs.count
   end
