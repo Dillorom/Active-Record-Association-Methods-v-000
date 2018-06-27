7ba4a737a8b0bc4d410260971d211b5e1e764c81
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-27 20:50:12 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,8 +1,10 @@
+require 'pry'
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
+    binding.pry
     #return the genre of the artist's first saved song
     self.songs.genres.first
   end
